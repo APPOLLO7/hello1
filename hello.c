0a15e3c042de95ca5b2ad372cf9c20602a60be32
Justin <justinkerr@huskers.unl.edu>
2019-08-29 21:23:25 -0500

    added majors

--- hello.c
+++ hello.c
@@ -3,6 +3,9 @@
 
 int main(int argc, char**argy); {
 
+    printf("Apollinaire Amomba\n");
+    printf("computer science\n");
+    printf("maths\n");
 printf("Justin Kerr\n");
 printf("Computer Engineering\n");
 printf("Physics\n");
