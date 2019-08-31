1c50a4de372ce61f937e0b2b5a58929c2235cf33
APPOLLO7 <54381887+APPOLLO7@users.noreply.github.com>
2019-08-30 20:23:52 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char**argy); {
+int main(int argc, char **argy) {
 
     printf("Apollinaire Amomba\n");
     printf("computer science\n");
     printf("maths\n");
-printf("Justin Kerr\n");
-printf("Computer Engineering\n");
-printf("Physics\n");
+    printf("Justin Kerr\n");
+    printf("Computer Engineering\n");
+    printf("Physics\n");
 
 return 0;
+}
